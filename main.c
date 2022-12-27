9f45dede410175181b8c05c5229ae3e6be78bf6f
Mauricio ML <mau_ml@comunidad.unam.mx>
2022-12-26 19:12:04 -0600

    Añadiendo funcion que checa si existe un usario

--- main.c
+++ main.c
@@ -1,27 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //#include <auth.c>
 
-
 FILE *usuarios;
-int registrar() { // Registrar un usuario.
 
+int existeUsuario(char user[]) { // Checa si existe un usuario.
+    char usArch[10];
+    int existe=0;
+
+    usuarios=fopen("users.txt", "r");
+    
+    if(!usuarios) {
+        printf("No existe el archivo \"users.txt\"");
+        return 0;
+    }
+    
+    while(!feof(usuarios)) {
+        fscanf(usuarios,"%s", usArch);
+        if(!strcmp(user, usArch))
+            existe++;      
+    }
+
+    fclose(usuarios); 
+    return existe;
+}
+
+int registrar() { // Registrar un usuario.
     char user[10], pass[10];
 
-	printf("Escribe tu nombre de usuario: ");
-    fgets(usuarios, 20, stdin);
+    do {
+        if (existeUsuario(user))
+            printf("\nEl usuario \"%s\" ya se encuentra registrado\n", user);
+        printf("Escribe tu nombre de usuario: ");
+        scanf("%10s", user);
+    }
+    while (existeUsuario(user));
 
-    printf("Escribe tu contraseña: ");
-    scanf("%s", pass);
+    if (existeUsuario(user))
+        printf("El usuario ya está registrado");
+    printf("Escribe tu contraseña (max 10 caracteres): ");
+    scanf("%10s", pass);
 
-	bd=fopen("users.txt", "a");
-	fprintf(bd,"%s\t%s", user,pass);
-    fclose(bd);
+	usuarios=fopen("users.txt", "a");
+	fprintf(usuarios,"%s\t%s\n", user, pass);
+    fclose(usuarios);
 
 	return 1;
 }
 
 int main(void) {
-	registrar();
+    registrar();
 	return 0;
 }
